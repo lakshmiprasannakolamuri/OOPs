c2c1d3e1599ad426bd28dfa85810b1c5dc5d0082
LP <lakshmiprasannakolamuri3@gmail.com>
2021-12-27 23:29:36 +0530

    create a constructor

--- OOPs/src/com/javapratice/Objects.java
+++ OOPs/src/com/javapratice/Objects.java
@@ -1,28 +1,37 @@
 package com.javapratice;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Objects
 {
 
-	Scanner scan = new Scanner(System.in);
+	//Scanner scan = new Scanner(System.in);
 
-	String name = "";
-	int marks = 0;
+	String myname ;
+	int mymarks ;
+	
+	//creating Constructor
+     Objects(String name, int marks){
+		 myname = name;
+		 mymarks = marks;
+		 
+	 }
 
 	public void get_details()
 	{
-		System.out.println("Enter the name:");
+		
+	
+	/*	System.out.println("Enter the name:");
 		name = scan.nextLine();
 
 		System.out.println("Enter the marks:");
-		marks = scan.nextInt();
+		marks = scan.nextInt();*/
 
 	}
 
 	public void show_details()
 	{
-		System.out.println("Name of the student" + name);
-		System.out.println("Marks of the student" + marks);
+		System.out.println("Name of the student" + myname);
+		System.out.println("Marks of the student" + mymarks);
 	}
 }
--- OOPs/src/com/javapratice/Students.java
+++ OOPs/src/com/javapratice/Students.java
@@ -5,11 +5,10 @@ public class Students
 
 	public static void main(String[] args)
 	{
-		Objects obj = new Objects();
+		Objects obj = new Objects("prasanna", 80);
 
 		obj.get_details();
 
 		obj.show_details();
 	}
-
 }
